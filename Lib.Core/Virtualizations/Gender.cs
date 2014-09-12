75bc60150a56e121c062dc593a4877993f328356
Wang Yucai <Yucai.Wang@Qq.com>
2014-09-12 16:47:33 +0800

    新增了Gender枚举类型。此枚举类型定义了常用的性别。

---
+++ Lib.Core/Virtualizations/Gender.cs
@@ -0,0 +1,70 @@
+#region "Gender"
+
+/*
+ * ####     Developer Name : Wang Yucai
+ * 
+ * ####     Development Tool : Microsoft VisualStudio 2010 Ultimate Edition
+ * 
+ * ####     Create Time : 2014-09-12 16:43:27
+ * 
+ * ####     Namespace : SourcefansStudio.FoundationFramework.Commons.Virtualizations
+ * 
+ * ####     Type Name : Gender
+ * 
+ * ####     Full Name : SourcefansStudio.FoundationFramework.Commons.Virtualizations.Gender
+ * 
+ * ####     Machine Name : GLCHQWYCWINW7
+ * 
+ * ####     UserName : GUOCOLAND/wangyucai
+ * 
+ * ####     CLR Version : 4.0.30319.18444
+ * 
+ * ####     Target Framework Version : 4.0
+ */
+
+#endregion
+
+using System;
+
+namespace SourcefansStudio.FoundationFramework.Commons.Virtualizations
+{
+    /// <summary>
+    /// <para>SourcefansStudio.FoundationFramework.Commons.Virtualizations.Gender</para>
+    /// <para>
+    /// 性别枚举。
+    /// </para>
+    /// </summary>
+    /// <remarks>
+    /// <para>Target Framework Version : 4.0</para>
+    /// </remarks>
+    [Serializable()]
+    public enum Gender
+    {
+        /// <summary>
+        /// 男（雄）性的。
+        /// </summary>
+        Male = 'M',
+        /// <summary>
+        /// 女（雌）性的。
+        /// </summary>
+        Female = 'F',
+        /// <summary>
+        /// 未知的。
+        /// </summary>
+        Unknown = 'U'
+    }
+}
+
+#region README
+
+/*
+ * ####     ©2014 Wang Yucai
+ * 
+ * ####     Contact me
+ *                  
+ *                  E-mail : Masterduner@Yeah.net
+ *                  Mobile : 13621384677
+ *                  QQ : 180261899
+ */
+
+#endregion
