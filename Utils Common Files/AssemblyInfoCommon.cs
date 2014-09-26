6c36d4eddeea59fadfd0cb73e2077903d4133346
Wang Yucai <Yucai.Wang@Qq.com>
2014-09-26 14:27:16 +0800

    新增了版本号创建工具。versionc

---
+++ Utils Common Files/AssemblyInfoCommon.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("Sourcefans Studio")]
+[assembly: AssemblyProduct("Sourcefans Studio (OpenSources) ---- Utility")]
+[assembly: AssemblyCopyright("Copyright © Wang Yucai 2014")]
+[assembly: AssemblyTrademark("Wang Yucai, A Csharp Programmer.")]
+
+[assembly: AssemblyCulture("")]
